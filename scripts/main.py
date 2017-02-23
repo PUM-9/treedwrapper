6dd14918d9c598e0f39049b54a209354fbc5f043
Ubuntu i rummet <nej@nej>
2017-02-23 10:17:03 +0100

    Treedwrapper is done

--- scripts/main.py
+++ scripts/main.py
@@ -3,11 +3,10 @@
 import rospy
 import os
 import pcl
-import time
 from treedwrapper.srv import WrapperScan, WrapperScanResponse
 import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2
-from watchdog import Watchdog, hardwareErrorOccur
+import pexpect
 
 def wrapper_scan(req):
     """
@@ -16,40 +15,41 @@ def wrapper_scan(req):
     :param req: (ROS request) The request with defined angles
     :return: (ROS response) Response with pcl object if everything went ok otherwise exit code 1 and error message.
     """
-
     x = req.x_angle
     y = req.y_angle
     default_file_path = "/tmp/recentscan.pcd"
 
     # Check if the X angle is allowed.
-    if is_x_angle_valid(x):
+    if is_valid_x_angle(x):
         return WrapperScanResponse(None, 1, "X value is not allowed, should be between 0 and 359")
 
     # Check if the Y angle is allowed.
-    if is_y_angle_valid(y):
+    if is_valid_y_angle(y):
         return WrapperScanResponse(None, 1, "Y value is not allowed, should be between -20 and 90")
-
-    # Create a watchdog object that start a timer and confirm that the scan would be successfully completed.
-    watchdog_scan = Watchdog(5)
+        
+    # Rotate the object and table.
     os.system("treed set --table-rotation " + str(y))
     os.system("treed set --table-curve " + str(x))
-    os.system("treed scan -o " + default_file_path)
-    watchdog_scan.stop()
-
-    # Check if the global variable in watchdog.py file has been set or not, to detect hardware error.
-    if hardwareErrorOccur:
+    
+    # Confirm that a scan would be successfully completed.
+    try:
+        # Starting a child process running the scan command.
+        child_process = pexpect.spawn ("treed scan -o " + default_file_path)
+        # Timeouts after 40 seconds.
+        child_process.expect('File saved to ' + default_file_path, timeout = 40)
+    except pexpect.TIMEOUT:
         return WrapperScanResponse(None, 1, "There is something wrong with the hardware")
 
     # Load in all the gathered points into a numpy array.
-    p = pcl.load(default_file_path)
+    points = pcl.load(default_file_path)
 
     # Convert points gathered from scan to PointCloud2 object.
-    pcloud = PointCloud2()
-    pcloud = pc2.create_cloud_xyz32(pcloud.header, p.to_list())
+    point_cloud = PointCloud2()
+    point_cloud = pc2.create_cloud_xyz32(point_cloud.header, points.to_list())
 
-    return WrapperScanResponse(pcloud, 0, "")
+    return WrapperScanResponse(point_cloud, 0, "")
 
-def is_x_angle_valid(x):
+def is_valid_x_angle(x):
     """
     Check if the angle for curve the table (x) upward/downward is allowed.
     :param x: -20 to 90
@@ -57,7 +57,7 @@ def is_x_angle_valid(x):
     """
     return x < -20 or x > 90
 
-def is_y_angle_valid(y):
+def is_valid_y_angle(y):
     """
     Check if the angle for rotating the board (y) is allowed.
     :param y: 0 to 359
@@ -70,8 +70,8 @@ def main():
     Spins up the WrapperScan service.
     :return: None
     """
-    rospy.init_node('treedwrapper', anonymous=False)
-    rospy.Service('wrapperscan', WrapperScan, wrapper_scan)
+    rospy.init_node('treed_wrapper', anonymous=False)
+    rospy.Service('wrapper_scan', WrapperScan, wrapper_scan)
     rospy.spin()
 
 if __name__ == '__main__':
