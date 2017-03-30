0a74622b57fc1f96665c20e576064bc30d1e4860
Hampus Dunström <hampusdunstrom@gmail.com>
2017-03-30 14:44:45 +0200

    added so all commands runs with pexpect and timeout also fixed minor bug and wrapped all cli cmds in try catch so we get better error messages.

--- scripts/main.py
+++ scripts/main.py
@@ -47,35 +47,30 @@ def wrapper_scan(req):
     if not is_valid_y_angle(y):
         return WrapperScanResponse(None, 1, "Y value is not allowed, should be between 0 and 359. The input value was "
                                    + str(y) + ".")
-        
-    # Rotate the object and table and set the speed of the camera.
-    output = subprocess.Popen(['treed', 'set', '--cart-speed', '200'], stdout=subprocess.PIPE).communicate()
-    print "cart speed"
-    print output
-    output = subprocess.Popen(['treed', 'set', '--table-rotation', str(y)], stdout=subprocess.PIPE).communicate()
-    print "set rotation"
-    print output
-    output = subprocess.Popen(['treed', 'set', '--table-curve', str(x)], stdout=subprocess.PIPE).communicate()
-    print "set curve"
-    print output
+
+    def run_child(cmd, expected_output, timeout):
+        child = pexpect.spawn(cmd)
+        child.expect(expected_output, timeout=timeout)
+        child.close()
 
     try:
+        # Rotate the object and table and set the speed of the camera.
+        run_child('treed set --cart-speed 200', '', 10)
+        run_child('treed set --table-rotation ' + str(y), '', 20)
+        run_child('treed set --table-curve ' + str(x), '', 20)
+
         # Starting a child process running the scan command.
-        child_process = pexpect.spawn("treed scan -o " + default_file_path)
-        # Timeouts after 40 seconds.
-        child_process.expect('File saved to ' + default_file_path, timeout=40)
+        run_child("treed scan -o " + default_file_path, 'File saved to ' + default_file_path, 40)
     except (pexpect.TIMEOUT, Exception), e:
         return WrapperScanResponse(None, 1, "There is something wrong with the hardware:\n " + str(e))
-
 
     # Load in all the gathered points into a numpy array.
     points = pcl.load(default_file_path)
 
     # Check that there are enough points
-    points_size = points.size()
     minimum_size = 50
-    if points_size < minimum_size:
-        return WrapperScanResponse(None, 1, "Not enough points only " + str(points_size) +
+    if points.size < minimum_size:
+        return WrapperScanResponse(None, 1, "Not enough points only " + str(points.size) +
                                    " point must be atleast " + str(minimum_size) + ".")
 
     # Convert points gathered from scan to PointCloud2 object.
